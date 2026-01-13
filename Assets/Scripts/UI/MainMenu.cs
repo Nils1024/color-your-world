ed82875f09b86227c7557e43040cfee51098c85f
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-01-13 22:47:13 +0100

    Added functional main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
