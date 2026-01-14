f3c4e1e6ca37a75e57193fc2fe60bfedf63208fb
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-01-14 20:52:07 +0100

    Refactoring

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,84 +1,84 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
-public class Movement : MonoBehaviour
+namespace Player
 {
-    public Camera playerCamera;
-    public float walkSpeed = 6f;
-    public float runSpeed = 12f;
-    public float jumpPower = 7f;
-    public float gravity = 10f;
-    public float lookSpeed = 1f;
-    public float lookXLimit = 45f;
-    public float defaultHeight = 2f;
-    public float crouchHeight = 1f;
-    public float crouchSpeed = 3f;
-
-    private Vector3 moveDirection = Vector3.zero;
-    private float rotationX = 0;
-    private CharacterController characterController;
-    private bool canMove = true;
-
-    void Start()
+    [RequireComponent(typeof(CharacterController))]
+    public class Movement : MonoBehaviour
     {
-        characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+        public Camera playerCamera;
+        public float walkSpeed = 6f;
+        public float runSpeed = 12f;
+        public float jumpPower = 7f;
+        public float gravity = 10f;
+        public float lookSpeed = 1f;
+        public float lookXLimit = 45f;
 
-    void Update()
-    {
-        var keyboard = Keyboard.current;
-        var mouse = Mouse.current;
+        private Vector3 _moveDirection = Vector3.zero;
+        private float _rotationX;
+        private CharacterController _characterController;
+        private const bool CanMove = true;
 
-        Vector3 forward = transform.forward;
-        Vector3 right = transform.right;
-
-        bool isRunning = keyboard.leftShiftKey.isPressed;
-
-        float verticalInput =
-            (keyboard.wKey.isPressed ? 1 : 0) -
-            (keyboard.sKey.isPressed ? 1 : 0);
-
-        float horizontalInput =
-            (keyboard.dKey.isPressed ? 1 : 0) -
-            (keyboard.aKey.isPressed ? 1 : 0);
-
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * verticalInput : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * horizontalInput : 0;
-
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-        // Jump
-        if (keyboard.spaceKey.wasPressedThisFrame && canMove && characterController.isGrounded)
+        private void Start()
         {
-            moveDirection.y = jumpPower;
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY;
+            _characterController = GetComponent<CharacterController>();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
 
-        // Gravity
-        if (!characterController.isGrounded)
+        private void Update()
         {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
+            var keyboard = Keyboard.current;
+            var mouse = Mouse.current;
 
-        characterController.Move(moveDirection * Time.deltaTime);
+            Vector3 forward = transform.forward;
+            Vector3 right = transform.right;
 
-        // Mouse look
-        if (canMove)
-        {
-            Vector2 mouseDelta = mouse.delta.ReadValue();
+            bool isRunning = keyboard.leftShiftKey.isPressed;
 
-            rotationX += -mouseDelta.y * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            float verticalInput =
+                (keyboard.wKey.isPressed ? 1 : 0) -
+                (keyboard.sKey.isPressed ? 1 : 0);
 
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, mouseDelta.x * lookSpeed, 0);
+            float horizontalInput =
+                (keyboard.dKey.isPressed ? 1 : 0) -
+                (keyboard.aKey.isPressed ? 1 : 0);
+
+            float curSpeedX = (isRunning ? runSpeed : walkSpeed) * verticalInput;
+            float curSpeedY = (isRunning ? runSpeed : walkSpeed) * horizontalInput;
+
+            float movementDirectionY = _moveDirection.y;
+            _moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            // Jump
+            if (keyboard.spaceKey.wasPressedThisFrame && CanMove && _characterController.isGrounded)
+            {
+                _moveDirection.y = jumpPower;
+            }
+            else
+            {
+                _moveDirection.y = movementDirectionY;
+            }
+
+            // Gravity
+            if (!_characterController.isGrounded)
+            {
+                _moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            _characterController.Move(_moveDirection * Time.deltaTime);
+
+            // Mouse look
+            if (CanMove)
+            {
+                Vector2 mouseDelta = mouse.delta.ReadValue();
+
+                _rotationX += -mouseDelta.y * lookSpeed;
+                _rotationX = Mathf.Clamp(_rotationX, -lookXLimit, lookXLimit);
+
+                playerCamera.transform.localRotation = Quaternion.Euler(_rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, mouseDelta.x * lookSpeed, 0);
+            }
         }
     }
 }
