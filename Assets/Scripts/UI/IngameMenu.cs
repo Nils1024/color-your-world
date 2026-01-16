c6f6c9e2e1fe34cfd440c01c925a6128c7d8be03
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-01-16 23:11:23 +0100

    - Finished the building the House - Changed the lightning (and baked it) - Aadded a hint button

--- Assets/Scripts/UI/IngameMenu.cs
+++ Assets/Scripts/UI/IngameMenu.cs
@@ -3,47 +3,56 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class IngameMenu : MonoBehaviour
+namespace  UI
 {
-    public GameObject ingameMenu;
-    public Movement playerMovement;
+    public class IngameMenu : MonoBehaviour
+    {
+        public GameObject ingameMenu;
+        public GameObject map;
+        public Movement playerMovement;
     
-    private Keyboard _keyboard = Keyboard.current;
+        private Keyboard _keyboard = Keyboard.current;
     
-    public void ResumeGame()
-    {
-        ingameMenu.SetActive(false);
-        playerMovement.isLocked = false;
-    }
-
-    public void ShowLeftoverObject()
-    {
-        
-    }
-
-    public void BackToMainMenu()
-    {
-        SceneManager.LoadSceneAsync(1);
-    }
-
-    private void Update()
-    {
-        if(_keyboard.escapeKey.wasPressedThisFrame)
+        public void ResumeGame()
         {
-            ingameMenu.SetActive(!ingameMenu.activeSelf);
+            ingameMenu.SetActive(false);
+            playerMovement.isLocked = false;
+        }
 
-            if(ingameMenu.activeSelf)
+        public void SaveGame()
+        {
+            Debug.Log("Save Game");
+        }
+    
+        public void BackToMainMenu()
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+
+        public void ShowHint()
+        {
+        
+        }
+
+        private void Update()
+        {
+            if(_keyboard.escapeKey.wasPressedThisFrame)
             {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
+                ingameMenu.SetActive(!ingameMenu.activeSelf);
+
+                if(ingameMenu.activeSelf)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                }
             
-            playerMovement.isLocked = ingameMenu.activeSelf;
+                playerMovement.isLocked = ingameMenu.activeSelf;
+            }
         }
     }
 }
