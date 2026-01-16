d9a0216641076eceea8adfaa58dbf43ff76b54ee
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-01-17 00:08:13 +0100

    Animated the main scene camera to fly into the house

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,17 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public CinemachineCamera vcam;
+    
     public void playGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        //SceneManager.LoadSceneAsync(0);
+        mainMenu.SetActive(false);
+        vcam.gameObject.SetActive(true);
     }
 
     public void quitGame()
