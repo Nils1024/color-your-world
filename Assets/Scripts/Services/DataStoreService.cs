84487cc1ac513bfc17c56b960360933dd2b52369
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-01-25 02:46:24 +0100

    Added a basketball court and a playground

---
+++ Assets/Scripts/Services/DataStoreService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Services
+{
+    public class DataStoreService : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        private void OnApplicationFocus(bool focus)
+        {
+            //TODO: Save (It should save when we lose focus to prevent data loss)
+        }
+        
+        private void OnApplicationQuit()
+        {
+            //TODO: Save
+        }
+    }
+}
