36f71ce4991611dbe4e24d55ca76a75ef1fdd9ca
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-02-01 02:31:55 +0100

    Added that save data is getting saved on the file system while playing in the editor

--- Assets/Scripts/Services/DataStoreService.cs
+++ Assets/Scripts/Services/DataStoreService.cs
@@ -6,6 +6,10 @@ namespace Services
 {
     public class DataStoreService : MonoBehaviour
     {
+        #if UNITY_EDITOR
+            private const string SaveDataFileName = "/DATA.json";
+        #endif
+        
         private const string PlayerPrefsDataKey = "Color-Your-World-SaveData";
         private SaveData _saveData = new();
         private static DataStoreService _instance;
@@ -19,9 +23,8 @@ namespace Services
         {
             ReadSaveData();
             
-            if (_instance != null && _instance != this)
+            if (_instance != null)
             {
-                Destroy(gameObject);
                 return;
             }
 
@@ -44,12 +47,25 @@ namespace Services
 
         private void WriteSaveData()
         {
-            PlayerPrefs.SetString(PlayerPrefsDataKey, JsonUtility.ToJson(_saveData));
+            #if UNITY_EDITOR
+                string filePathSaveData = Application.persistentDataPath + SaveDataFileName;
+                    
+                string txt = JsonUtility.ToJson(_saveData);
+                File.WriteAllText(filePathSaveData, txt);
+            #else
+                PlayerPrefs.SetString(PlayerPrefsDataKey, JsonUtility.ToJson(_saveData));
+            #endif
         }
 
         private void ReadSaveData()
         {
-            _saveData = JsonUtility.FromJson<SaveData>(PlayerPrefs.GetString(PlayerPrefsDataKey));
+            #if UNITY_EDITOR
+                string filePath = Application.persistentDataPath + DataStoreService.SaveDataFileName;
+                string fileContent = File.ReadAllText(filePath);
+                _saveData = JsonUtility.FromJson<SaveData>(fileContent);
+            #else
+                _saveData = JsonUtility.FromJson<SaveData>(PlayerPrefs.GetString(PlayerPrefsDataKey));
+            #endif
         }
 
         public SaveData GetSaveData()
